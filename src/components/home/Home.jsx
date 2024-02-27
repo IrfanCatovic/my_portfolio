3115c9cb66f1336bc2d363ef1abf463f455e954f
IrfanCatovic <catovicc84@gmail.com>
2024-02-27 12:00:53 +0100

    home and selfie added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,18 @@
 import "./home.css";
+import Me from "../../assests/selfie.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home_img" />
+        <h1 className="home_name">Irfan Ćatović</h1>
+        <span className="home_education">
+          I'm a full stack developer and graphic designer
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
