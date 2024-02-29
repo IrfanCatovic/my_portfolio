1a6e39b4679b77e45c6143865568b694b8be4c4a
IrfanCatovic <catovicc84@gmail.com>
2024-02-29 12:25:42 +0100

    Resume and exp done

--- src/components/resume/Data.jsx
+++ src/components/resume/Data.jsx
@@ -32,28 +32,29 @@ const Data = [
     desc: "C# programming Certificate by Claster NIT",
   },
   {
+    id: 5,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2019 - 2020 ",
+    title: "Back end developer .NET ",
+    desc: "Klaster NIT Novi Pazar -  I collaborated with a talented team of developers, soaking in knowledge and honing my skills in the dynamic, collaborative environment, contributing to the seamless creation of solutions that navigate the digital world with ease. ",
+  },
+  {
     id: 4,
     category: "experience",
     icon: "icon-briefcase",
     year: "2020 - 2023",
     title: "Graphic designer - video editor",
-    desc: "Studio Hane Luxembourg",
+    desc: "Studio Hane Luxembourg - I shaped captivating visual stories, guiding the post-production process to deliver compelling content that consistently exceeded client expectations.",
   },
-  {
-    id: 5,
-    category: "experience",
-    icon: "icon-briefcase",
-    year: "2019 - 2020 ",
-    title: "Back end developer C#",
-    desc: "Klaster NIT - Novi Pazar ",
-  },
+
   {
     id: 6,
     category: "experience",
     icon: "icon-briefcase",
     year: "2023 - present",
     title: "Full Stack Developer React/.NET ",
-    desc: "ILM education center",
+    desc: "ILM education center - I surf the waves of frontend innovation, working alongside a vibrant team to cultivate engaging user interfaces and contribute to the collective knowledge, turning every coding challenge into an opportunity for growth.",
   },
 ];
 
