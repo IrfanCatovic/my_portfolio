151a65d7de18a7132880bd9ff7a4474d0e9bad27
IrfanCatovic <catovicc84@gmail.com>
2024-02-29 16:41:45 +0100

    Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,42 @@
-import "./portfolio.css"
+import "./portfolio.css";
+import Menu from "./Menu";
+import { useState } from "react";
 
 function Portfolio() {
-    return (
-        <div>
-            Portfolio
-        </div>
-    )
+  const [items, setItems] = useState(Menu);
+
+  return (
+    <section className="work container section" id="work">
+      <h2 className="section_title">Recent Works</h2>
+
+      <div className="work_filters">
+        <span className="work_item">Everything</span>
+        <span className="work_item">Creative</span>
+        <span className="work_item">Art</span>
+        <span className="work_item">Design</span>
+        <span className="work_item">Branding</span>
+      </div>
+
+      <div className="work_container grid">
+        {items.map((elem) => {
+          const { id, image, title, category } = elem;
+          return (
+            <div className="work_card" key={id}>
+              <div className="work_thumbnail">
+                <img src={image} alt="" className="work_img" />
+                <div className="work_mask"></div>
+
+                <span className="work_category">{category}</span>
+                <h3 className="work_title">{title}</h3>
+                <a href="#" className="work_button"></a>
+                <i className="icon-link work_button"></i>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
